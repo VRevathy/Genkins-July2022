951044d11b427caab08ee6ad32deaac44ecef72a
Revathy Dhiren <vrevathy@gmail.com>
2022-07-23 06:55:06 +0100

    Commiting the project first Time

---
+++ src/test/java/TestApplication.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+public class TestApplication {
+    @Test
+    public void test1(){
+        System.out.println("Running Test1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Running Test2");
+    }
+    @Test
+    public void test3(){
+        System.out.println("Running Test3");
+    }
+    @Test
+    public void test4(){
+        System.out.println("Running Test4");
+    }
+}
